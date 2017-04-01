985b7b5b3408b13251bbeca634cdc7dc2b673dda
klepchella <klepchella@gmail.com>
2017-04-01 22:36:52 +0400

    remove static fields, some optimisation

--- Cell.py
+++ Cell.py
@@ -4,18 +4,12 @@ import Board
 
 class Cell:
 
-    xCoord = None
-    yCoord = None
-    size = None
-    currentState = None
-    nextState = None
-    neighbors = None
-
     def __init__(self, boardWidth, i, j, stateCell = 0, cellSize = 10):
         self.xCoord = i
         self.yCoord = j
         self.currentState = stateCell
         self.size = cellSize
+        self.neighbors = 0
 
     def lifeLaw(self):
         if self.neighbors == 2:
